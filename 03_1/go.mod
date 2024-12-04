7f35f2ec33452ed78c64903c896989ed0663b24c
TamimEhsan <tamimehsan99@gmail.com>
2024-12-04 11:37:15 +0600

    day 3

---
+++ 03_1/go.mod
@@ -0,0 +1,3 @@
+module github.com/tamimehsan/AoC2024/03_1
+
+go 1.21.5
---
+++ 03_2/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func input(muls *[]string) {
+	/**
+	take input from file, the inputs are list a pair of integer
+	*/
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	pattern := "do\\(\\)|don't\\(\\)|mul\\([0-9]+,[0-9]+\\)"
+	r, _ := regexp.Compile(pattern)
+	for scanner.Scan() {
+		line := scanner.Text()
+		match := r.FindAllString(line, -1)
+		*muls = append(*muls, match...)
+	}
+}
+
+func extractNumbers(s string) (int, int) {
+	/**
+	extract two numbers from the string
+	*/
+	r, _ := regexp.Compile("[0-9]+")
+	nums := r.FindAllString(s, -1)
+	num1, _ := strconv.Atoi(nums[0])
+	num2, _ := strconv.Atoi(nums[1])
+	return num1, num2
+}
+
+func calcResult(muls *[]string) int {
+	/**
+	calculate the result of the multiplication
+	*/
+	result := 0
+	state := true
+	for _, mul := range *muls {
+		if mul == "do()" {
+			state = true
+		} else if mul == "don't()" {
+			state = false
+		} else if state {
+			num1, num2 := extractNumbers(mul)
+			result += num1 * num2
+		}
+	}
+	return result
+}
+func main() {
+	// declare empty slice
+	var muls []string
+
+	// take input from file
+	input(&muls)
+
+	// calculate the result
+	res := calcResult(&muls)
+
+	fmt.Println(res)
+}
